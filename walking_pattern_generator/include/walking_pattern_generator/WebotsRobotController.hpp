5651cfb070b239a7582a6bf431ee4dac9f771638
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2022-10-09 19:02:24 +0900

    draft framework code.

---
+++ walking_pattern_generator/include/walking_pattern_generator/WebotsRobotController.hpp
@@ -0,0 +1,13 @@
+#include "rclcpp/rclcpp.hpp"
+#include "webots_ros2_driver/PluginInterface.hpp"
+#include "webots_ros2_driver/WebotsNode.hpp"
+
+namespace walking_pattern_generator
+{
+    class WebotsRobotController : public webots_ros2_driver::PluginInterface
+    {
+    public:
+        void step() override;
+        void init(webots_ros2_driver::WebotsNode *node, std::unordered_map<std::string, std::string> &parameters) override;
+    };
+}
---
+++ walking_pattern_generator/src/WebotsRobotController.cpp
@@ -0,0 +1,27 @@
+#include "walking_pattern_generator/WebotsRobotController.hpp"
+#include "pluginlib/class_list_macros.hpp"
+
+namespace walking_pattern_generator
+{
+    void WebotsRobotController::init(
+        webots_ros2_driver::WebotsNode *node, 
+        std::unordered_map<std::string, std::string> &parameters
+    ){
+        int argc = 0;
+        char const * const argv[] = {""};
+        rclcpp::init(argc, argv);
+        auto ros2_node = rclcpp::Node::make_shared("robot_controller");
+
+        auto robot = node->robot();
+        RCLCPP_INFO(ros2_node->get_logger(), "Hello my mine...");
+    }
+
+    void WebotsRobotController::step()
+    {
+        std::cout << "Test, test, test..." << std::endl;
+    }
+}
+
+PLUGINLIB_EXPORT_CLASS(
+    walking_pattern_generator::WebotsRobotController, webots_ros2_driver::PluginInterface
+)
