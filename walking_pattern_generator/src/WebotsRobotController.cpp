d18c0b6d1d7a0d39d8c7063653782109fbfcbbef
YYusuke-555 <91410662+Yusuke-Yamasaki-555@users.noreply.github.com>
2022-10-09 23:24:04 +0900

    Update WebotsRobotController.cpp

--- walking_pattern_generator/src/WebotsRobotController.cpp
+++ walking_pattern_generator/src/WebotsRobotController.cpp
@@ -6,9 +6,14 @@ namespace walking_pattern_generator {
         webots_ros2_driver::WebotsNode *node, 
         std::unordered_map<std::string, std::string> &parameters
     ) {
-        int argc = 0;
-        char const * const argv[] = {""};
-        rclcpp::init(argc, argv);
+        // ROS2の初期化方法がわからないため、強引にコンパイルを通過した方法。
+        //int argc = 0;
+        //char const * const argv[] = {""};
+        //rclcpp::init(argc, argv);
+        
+        // webots_ros2_driverのsensor周りを参考にすると、以下のような記述。恐らくこの記述も、ROS2の初期化となっているはず。（未検証。未コンパイル）
+        // PluginInterface::init(node, parameters);
+        
         auto ros2_node = rclcpp::Node::make_shared("robot_controller");
 
         auto robot = node->robot();
