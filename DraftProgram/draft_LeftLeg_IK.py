8987b43c68fd7dd47120c2dce92a5a1c2ece9eb5
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2022-12-17 22:13:22 +0900

    Update draft LeftLegKinematics (success)

--- DraftProgram/draft_LeftLeg_IK.py
+++ DraftProgram/draft_LeftLeg_IK.py
@@ -29,19 +29,21 @@ P23 = np.matrix([0, 0, 0])
 P34 = np.matrix([0, 0, -0.093])
 P45 = np.matrix([0, 0, -0.093])
 P56 = np.matrix([0, 0, 0])
-print(Pw1)
+# print(Pw1)
 
 # Target_Point
-# Pw6_target = Pw1 + P34
+# Pw6_target = Pw1 + P34 + P45/2
+# Pw6_target = Pw1 + P34 
+# Pw6_target = Pw1 + P34/2
 Rw6_target = identityMatrix()
-Pw6_target = np.array([-0.005, 0.037, -0.2152])
+Pw6_target = np.array([0.05, 0.1, -0.194])
 
 
 """ IK """
 qqq = Pw1 - Pw6_target
-print(qqq.T)
+# print(qqq.T)
 P16_target = np.array((Rw6_target * qqq.T).T)[0]
-print(P16_target)
+# print(P16_target)
 
 a = np.abs(P34[0,2])
 b = np.abs(P45[0,2])
@@ -49,12 +51,12 @@ c = np.sqrt(P16_target[0]**2 + P16_target[1]**2 + P16_target[2]**2)
 # print(a, b, c)
 
 Q3 = -np.arccos((a**2 + b**2 - c**2)/(2 * a * b)) + np.pi
-alfa = np.sin((a * np.sin(np.pi - Q3))/c)
+alfa = np.arcsin((a * np.sin(np.pi - Q3))/c)
 Q4 = -np.arctan2(P16_target[0], np.sign(P16_target[2]) * np.sqrt(P16_target[1]**2 + P16_target[2]**2)) - alfa
 Q5 = np.arctan2(P16_target[1], P16_target[2])
 # print(P16_target[1], P16_target[2], np.arctan2(P16_target[1], P16_target[2]))
 # When general Rw6_target
-R1_2_3 = np.dot(Rw6_target, np.dot(Rx(Q5), np.dot(Ry(Q4).T, Ry(Q3).T)))
+R1_2_3 = np.dot(Rw6_target, np.dot(Rx(Q5).T, np.dot(Ry(Q4).T, Ry(Q3).T)))
 Q0 = np.arctan2(-1*R1_2_3[0, 1], R1_2_3[1, 1])
 Q1 = np.arctan2(R1_2_3[2, 1], -1*R1_2_3[0, 1] * np.sin(Q0) + R1_2_3[1, 1] * np.cos(Q0))
 Q2 = np.arctan2(-1*R1_2_3[2, 0], R1_2_3[2, 2])
@@ -79,12 +81,12 @@ R345 = Ry(Q2+Q3+Q4)
 R24 = Ry(Q2+Q3)
 R56 = Rx(Q5)
 
-print(Rw1)
-print(R12)
-print(R23)
-print(R34)
-print(R45)
-print(R56)
+# print(Rw1)
+# print(R12)
+# print(R23)
+# print(R34)
+# print(R45)
+# print(R56)
 
 Pw1 = np.matrix([[-0.005], [0.037], [-0.1222]])
 P12 = np.matrix([[0], [0], [0]])
@@ -97,7 +99,7 @@ P6a = np.matrix([[0], [0], [0]])
 # FK_result = Rw1 @ R12 @ R23 @ R34 @ R45 @ R56 @ P6a + Rw1 @ R12 @ R23 @ R34 @ R45 @ P56 + Rw1 @ R12 @ R23 @ R34 @ P45 + Rw1 @ R12 @ R23 @ P34 + Rw1 @ R12 @ P23 + Rw1 @ P12 + Pw1
 # FK_result = Rw1@R12@R23@R34@P45 + Rw1@R12@R23@P34 + Pw1
 FK_result = Rw1 @ R12 @ R24 @ P45 + Rw1 @ R12 @ R23 @ P34 + Pw1
-print(Rw1 @ R12 @ R24 @ P45, Rw1 @ R12 @ R23 @ P34)
+# print(Rw1 @ R12 @ R24 @ P45, Rw1 @ R12 @ R23 @ P34)
 # FK_result = np.dot(Rw1, np.dot(R12, np.dot(R23, np.dot(R34, P45)))) + np.dot(Rw1, np.dot(R12, np.dot(R23, P34))) + Pw1
 
-print("FK_result: ", FK_result)
+print("FK_result[m]: ", FK_result.T, "\n")
