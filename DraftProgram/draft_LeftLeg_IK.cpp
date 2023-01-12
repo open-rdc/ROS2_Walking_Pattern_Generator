c2f62785e07e28160669606b0952a5cf9905d89f
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2023-01-13 00:49:37 +0900

    Create file draft_LeftLeg_IK.cpp

---
+++ DraftProgram/draft_LeftLeg_IK.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+#include "Eigen/Dense"
+
+using Eigen::MatrixXd;
+
+int main() {
+    MatrixXd mat(2, 2);  // 空の行列を、サイズ指定で作成
+    mat(0, 0) = 1;
+    mat(0, 1) = 2;
+    mat(1, 0) = 3;
+    mat(1, 1) = mat(0, 0) + mat(0, 1);
+    std::cout << mat << std::endl;
+}
