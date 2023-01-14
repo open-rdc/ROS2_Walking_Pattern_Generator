a8f96f88e0d2e915cf7ac61db8f080454c2ee04b
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2023-01-15 04:18:11 +0900

    update draft_LeftLeg_IK.cpp

--- DraftProgram/draft_LeftLeg_IK.cpp
+++ DraftProgram/draft_LeftLeg_IK.cpp
@@ -1,6 +1,8 @@
 #include "iostream"
+#include "cmath"
 #include "Eigen/Dense"
 
+bool DEBUG = true;
 
 using namespace Eigen;
 using namespace std;
@@ -27,12 +29,70 @@ Matrix3d Rz(double theta) {
          0         , 0          , 1;
     return(R);
 }
+Matrix3d IdentifyMatrix(void) {
+    Matrix3d R;
+    R << 1, 0, 0,
+         0, 1, 0,
+         0, 0, 1;
+    return(R);
+}
+
+
+namespace Parameters {
+    Matrix3d R_target;
+    Vector3d P_target;
+
+    array<Vector3d, 6> P;
+
+    array<float, 6> Q;
+}
+void set_Parameters() {
+    using namespace Parameters;
+
+    array<Vector3d, 6> P_list;
+    
+    // パラメータの正式な取得方法は、は他ファイルに記述してそれを読み込む、 他nodeから読み込む
+    if(DEBUG == true) {
+        R_target = IdentifyMatrix();
+        P_target << -0.005, 0.037, -0.1687;
+
+        Vector3d P_w1(-0.005, 0.037, -0.1222);
+        Vector3d P_12(0, 0, 0);
+        Vector3d P_23(0, 0, 0);
+        Vector3d P_34(0, 0, -0.093);
+        Vector3d P_45(0, 0, -0.093);
+        Vector3d P_56(0, 0, 0);
+
+        P_list = {P_w1, P_12, P_23, P_34, P_45, P_56};
+    }
+    else if(DEBUG ==false) {
+        // 正式な処理
+    }
+
+    for(int i = 0; i < P.size(); i++) {
+        P[i] = P_list[i];  // ココ、わざわざfor loopにする必要はない。まんま代入でイケるはず
+    }
+}
 
 
 int main() {
-    cout << Rx(0) << endl;
-    cout << Ry(0) << endl;
-    cout << Rz(0) << endl;
+    using namespace Parameters;
+
+    bool SETUP;  // 他nodeからのフラグで決めたい
+    if(DEBUG == true) {SETUP = true;}
+    if(DEBUG == true || SETUP == true) {set_Parameters();}
+
+    array<float, 6> Q_list;
+    if(DEBUG == true) {Q_list = {0, 0, 0, 0, 0, 0};}
+    for(int i = 0; i < Q.size(); i++) {
+        Q[i] = Q_list[i];  // ココ、わざわざfor loopにする必要はない。まんま代入でイケるはず
+    }
+
+    // cout << Rx(3.14) << endl;
+    // cout << Ry(3.14) << endl;
+    // cout << Rz(3.14) << endl;
+
+    cout << R_target << endl;
     return(0);
 }
 
