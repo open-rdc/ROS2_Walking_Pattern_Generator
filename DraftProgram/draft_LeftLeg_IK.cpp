c3d807862c67b1a28418edc95f5d7c848ea03e0a
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2023-01-15 04:05:25 +0900

    update draft_LeftLeg_IK.cpp

--- DraftProgram/draft_LeftLeg_IK.cpp
+++ DraftProgram/draft_LeftLeg_IK.cpp
@@ -1,15 +1,27 @@
 #include "iostream"
 #include "Eigen/Dense"
 
-using Eigen::MatrixXd;
+using namespace Eigen;
+using namespace std;
 
 int main() {
-    MatrixXd mat(2, 2);  // 空の行列を、サイズ指定で作成
-    mat(0, 0) = 1;
-    mat(0, 1) = 2;
-    mat(1, 0) = 3;
-    mat(1, 1) = mat(0, 0) + mat(0, 1);
-    std::cout << mat << std::endl;
+    // MatrixXd mat(2, 2);  // 空の行列を、サイズ指定で作成 動的？
+    Matrix2d A, B;  // 静的？ 上と同じ
+    // mat(0, 0) = 1;
+    // mat(0, 1) = 2;
+    // mat(1, 0) = 3.5;
+    // mat(1, 1) = mat(0, 0) + mat(0, 1);
+    A << 1, 2,
+         3, 4;
+    B << 1, 0,
+         0, 1;
+    cout << A << endl;
+    cout << B << endl;
+    cout << A.transpose() << endl;
+    cout << B.transpose() << endl;
+    MatrixXd result_dot = A * B;
+    cout << "dot A, B: \n" << result_dot << endl;
+
 }
 
 /*
