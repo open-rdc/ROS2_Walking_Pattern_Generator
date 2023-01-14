2b2c74c9d629d468e28c19c92f06cb1f11d26208
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2023-01-15 00:11:16 +0900

    update draft_LeftLeg_IK.cpp ( rotation-matrix )

--- DraftProgram/draft_LeftLeg_IK.cpp
+++ DraftProgram/draft_LeftLeg_IK.cpp
@@ -1,10 +1,43 @@
 #include "iostream"
 #include "Eigen/Dense"
 
+
 using namespace Eigen;
 using namespace std;
 
+
+Matrix3d Rx(double theta) {
+    Matrix3d R;
+    R << 1, 0, 0,
+         0, cos(theta), -sin(theta),
+         0, sin(theta), cos(theta);
+    return(R);
+}
+Matrix3d Ry(double theta) {
+    Matrix3d R;
+    R << cos(theta) , 0, sin(theta),
+         0          , 1, 0         ,
+         -sin(theta), 0, cos(theta);
+    return(R);
+}
+Matrix3d Rz(double theta) {
+    Matrix3d R;
+    R << cos(theta), -sin(theta), 0,
+         sin(theta), cos(theta) , 0,
+         0         , 0          , 1;
+    return(R);
+}
+
+
 int main() {
+    cout << Rx(0) << endl;
+    cout << Ry(0) << endl;
+    cout << Rz(0) << endl;
+    return(0);
+}
+
+/*
+MEMO
     // MatrixXd mat(2, 2);  // 空の行列を、サイズ指定で作成 動的？
     Matrix2d A, B;  // 静的？ 上と同じ
     // mat(0, 0) = 1;
@@ -21,10 +54,4 @@ int main() {
     cout << B.transpose() << endl;
     MatrixXd result_dot = A * B;
     cout << "dot A, B: \n" << result_dot << endl;
-
-}
-
-/*
-参考
-・https://qiita.com/yohm/items/a03006790dc1e54a87be
 */
