949cd2dfa6e579da20a62bd47cb3c8b449445d37
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2023-03-29 16:57:21 +0900

    Add  TimerAction (delay fk) Result: Failed.

--- webots_robot_handler/launch/start_launch.py
+++ webots_robot_handler/launch/start_launch.py
@@ -6,8 +6,8 @@ from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher, Ros2SupervisorLauncher
 
 def generate_launch_description():
-  package_dir = get_package_share_directory("webots_robot_handler")
-  robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'webots_robotis_op2_description.urdf')).read_text()
+  # package_dir = get_package_share_directory("webots_robot_handler")
+  # robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'webots_robotis_op2_description.urdf')).read_text()
 
   # FK (kinematics)
   fk = Node(
@@ -17,13 +17,13 @@ def generate_launch_description():
     output = "screen",
   )
 
-  # IK (kinematics)
-  ik = Node(
-    package = "kinematics",
-    namespace = "kinematics",
-    executable = "ik_srv",
-    output = "screen",
-  )
+  # # IK (kinematics)
+  # ik = Node(
+  #   package = "kinematics",
+  #   namespace = "kinematics",
+  #   executable = "ik_srv",
+  #   output = "screen",
+  # )
 
   # walking_stabilization_controller
   walking_stabilization_controller = Node(
@@ -33,41 +33,43 @@ def generate_launch_description():
     output = "screen",
   )
 
-  # webots world
-  webots = WebotsLauncher(  
-    world = os.path.join(package_dir, "worlds", "webots_simple_world.wbt")
-  )
+  # # webots world
+  # webots = WebotsLauncher(  
+  #   world = os.path.join(package_dir, "worlds", "webots_simple_world.wbt")
+  # )
 
-  # webots_robot_handler (C++_plugin of webots_ros2_driver)
-  robotis_op2_driver = Node(
-    package = "webots_ros2_driver",
-    executable = "driver",
-    output = "screen",
-    additional_env = {"WEBOTS_CONTROLLER_URL": "ipc://1234/ROBOTIS_OP2"},
-    parameters = [
-      {"robot_description": robot_description}
-    ],
-  )
+  # # webots_robot_handler (C++_plugin of webots_ros2_driver)
+  # robotis_op2_driver = Node(
+  #   package = "webots_ros2_driver",
+  #   executable = "driver",
+  #   output = "screen",
+  #   additional_env = {"WEBOTS_CONTROLLER_URL": "ipc://1234/ROBOTIS_OP2"},
+  #   parameters = [
+  #     {"robot_description": robot_description}
+  #   ],
+  # )
 
-  # walking_pattern_generator
-  walking_pattern_generator = Node(
-    package = "walking_pattern_generator",
-    namespace = "walking_pattern_generator",
-    executable = "walking_pattern_generator",
-    output = "screen",
-  )
+  # # walking_pattern_generator
+  # walking_pattern_generator = Node(
+  #   package = "walking_pattern_generator",
+  #   namespace = "walking_pattern_generator",
+  #   executable = "walking_pattern_generator",
+  #   output = "screen",
+  # )
 
   return launch.LaunchDescription([
-    fk,
-    ik,
+    launch.actions.TimerAction(period=5.0, actions = [
+      fk,
+    ]),
+    # ik,
     walking_stabilization_controller,
-    webots,
-    robotis_op2_driver,
-    walking_pattern_generator,
-    launch.actions.RegisterEventHandler(
-      event_handler = launch.event_handlers.OnProcessExit(
-        target_action = webots,
-        on_exit = [launch.actions.EmitEvent(event = launch.events.Shutdown())],
-      )
-    )        
+    # webots,
+    # robotis_op2_driver,
+    # walking_pattern_generator,
+    # launch.actions.RegisterEventHandler(
+    #   event_handler = launch.event_handlers.OnProcessExit(
+    #     target_action = webots,
+    #     on_exit = [launch.actions.EmitEvent(event = launch.events.Shutdown())],
+    #   )
+    # )        
   ])
