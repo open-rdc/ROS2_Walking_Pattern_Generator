b7d8969514469d325226bb592541d900276114a5
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2023-04-17 14:04:51 +0900

    Update WPG

--- walking_pattern_generator/include/walking_pattern_generator/WalkingPatternGenerator.hpp
+++ walking_pattern_generator/include/walking_pattern_generator/WalkingPatternGenerator.hpp
@@ -27,11 +27,23 @@ namespace walking_pattern_generator
       bool publish_ok_check_;
       int step_counter_;
       int loop_number_;
-      // 逆運動学からJointAngleを導出する
-      std::array<double, 5> walking_pattern_x_;
-      std::array<double, 5> walking_pattern_y_;
-      Eigen::Vector3d walking_pattern_jointVel_x_;
-      Eigen::Vector3d walking_pattern_jointVel_y_;
+      
+      Eigen::Vector2d walking_pattern_init_com_;  // 初期重心位置(x, y)
+      Eigen::Vector2d walking_pattern_init_p_;  // 初期着地位置(x, y)
+      Eigen::Matrix<double, 2, 5> walking_pattern_s_;  // 歩行パラメータ(x1~5, y1~5)
+      Eigen::Vector2d walking_pattern_p_;  // 着地位置(x, y)
+      Eigen::Vector2d walking_pattern_p_old_;  // １step前の着地位置(x, y)
+      Eigen::Vector2d walking_segment_;  // 歩行素片位置(x, y)
+      Eigen::Vector2d walking_segment_vel_;  // 歩行素片速度(x, y)
+      Eigen::Vector2d walking_pattern_x_;  // n歩目の歩行状態のxの最終状態(x, dx)
+      Eigen::Vector2d walking_pattern_y_;  // 上に同じ(y, dy)
+      Eigen::Vector2d walking_pattern_xi_;  // n歩目が始まる瞬間のxの重心位置と速度(x, dx)
+      Eigen::Vector2d walking_pattern_yi_;  // 上に同じ(y, dy)
+      Eigen::Vector2d walking_pattern_target_x_;  // xの最終状態の目標値(x, dx)
+      Eigen::Vector2d walking_pattern_target_y_;  // 上に同じ(y, dy)
+
+
+
 
       void step_WPG_pub(void);
 
