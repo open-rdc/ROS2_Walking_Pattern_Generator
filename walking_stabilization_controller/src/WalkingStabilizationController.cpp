d2cb59f8c177a2a6432984b4cd60ba1b2c138a03
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2023-04-21 00:27:12 +0900

    update

--- walking_stabilization_controller/src/WalkingStabilizationController.cpp
+++ walking_stabilization_controller/src/WalkingStabilizationController.cpp
@@ -129,19 +129,19 @@ namespace walking_stabilization_controller
     //   toKine_FK_req, 
     //   std::bind(&WalkingStabilizationController::callback_res, this, _1)
     // );
-    auto toKine_IK_res = toKine_IK_clnt_->async_send_request(
-      toKine_IK_req,
-      std::bind(&WalkingStabilizationController::callback_res, this, _1)
-    );
+    // auto toKine_IK_res = toKine_IK_clnt_->async_send_request(
+    //   toKine_IK_req,
+    //   std::bind(&WalkingStabilizationController::callback_res, this, _1)
+    // );
 
-    response->q_fix_r = Q_fix_legR_;
-    response->q_fix_l = Q_fix_legL_;
-    response->dq_fix_r = dQ_fix_legR_;
-    response->dq_fix_l = dQ_fix_legL_;
-    // response->q_fix_r = Q_target_legR_;
-    // response->q_fix_l = Q_target_legL_;
-    // response->dq_fix_r = dQ_target_legR_;
-    // response->dq_fix_l = dQ_target_legL_;
+    // response->q_fix_r = Q_fix_legR_;
+    // response->q_fix_l = Q_fix_legL_;
+    // response->dq_fix_r = dQ_fix_legR_;
+    // response->dq_fix_l = dQ_fix_legL_;
+    response->q_fix_r = Q_target_legR_;
+    response->q_fix_l = Q_target_legL_;
+    response->dq_fix_r = dQ_target_legR_;
+    response->dq_fix_l = dQ_target_legL_;
 
     // RCLCPP_INFO(this->get_logger(), "Finish WSC_SrvServer");
   }
