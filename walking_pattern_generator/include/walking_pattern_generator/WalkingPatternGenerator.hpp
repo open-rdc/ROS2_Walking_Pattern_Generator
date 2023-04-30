6ddffe8de54c4898594adf2701f8a39d9fdc53a9
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2023-04-30 16:32:39 +0900

    Update WalkingPatternGenerator

--- walking_pattern_generator/include/walking_pattern_generator/WalkingPatternGenerator.hpp
+++ walking_pattern_generator/include/walking_pattern_generator/WalkingPatternGenerator.hpp
@@ -1,6 +1,10 @@
 #include "rclcpp/rclcpp.hpp"
-#include "msgs_package/msg/to_walking_stabilization_controller_message.hpp"
-#include "msgs_package/srv/to_kinematics_message.hpp"
+// #include "msgs_package/msg/to_walking_stabilization_controller_message.hpp"
+// #include "msgs_package/srv/to_kinematics_message.hpp"
+#include "msgs_package/srv/to_walking_pattern_generator.hpp"
+#include "kinematics/IK.hpp"
+
+#include "Eigen/Dense"
 
 namespace walking_pattern_generator
 {
@@ -9,9 +13,13 @@ namespace walking_pattern_generator
       WalkingPatternGenerator(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
 
     private:
-      rclcpp::Publisher<msgs_package::msg::ToWalkingStabilizationControllerMessage>::SharedPtr toWSC_pub_;
-      rclcpp::Client<msgs_package::srv::ToKinematicsMessage>::SharedPtr toKine_FK_clnt_;
-      rclcpp::Client<msgs_package::srv::ToKinematicsMessage>::SharedPtr toKine_IK_clnt_;
+      // rclcpp::Publisher<msgs_package::msg::ToWalkingStabilizationControllerMessage>::SharedPtr toWSC_pub_;
+      // rclcpp::Client<msgs_package::srv::ToKinematicsMessage>::SharedPtr toKine_FK_clnt_;
+      // rclcpp::Client<msgs_package::srv::ToKinematicsMessage>::SharedPtr toKine_IK_clnt_;
+
+      rclcpp::Service<msgs_package::srv::ToWalkingPatternGenerator>::SharedPtr WPG_srv_;
+
+      kinematics::IK IK_;
 
       // get FK, IK result. set publish data.
       std::array<double, 3> p_target_r_;
@@ -20,22 +28,27 @@ namespace walking_pattern_generator
       std::array<double, 6> q_target_l_;
 
       // timer
-      rclcpp::TimerBase::SharedPtr step_pub_;
+      // rclcpp::TimerBase::SharedPtr step_pub_;
 
       // PARAMETER
-      bool publish_ok_check_;
-      int step_counter_;
-      int loop_number_;
+      // bool publish_ok_check_;
+      // int step_counter_;
+      // int loop_number_;
+      int step_count_;
       // 逆運動学からJointAngleを導出する
       std::array<std::array<double, 3>, 4> walking_pattern_P_R_;
       std::array<std::array<double, 3>, 4> walking_pattern_P_L_;
-      // jointVelも、逆動力学（？）で導出したい。
       std::array<std::array<double, 6>, 4> walking_pattern_jointVel_R_;
       std::array<std::array<double, 6>, 4> walking_pattern_jointVel_L_;
 
-      void step_WPG_pub(void);
+      void WPG_Server(
+        const std::shared_ptr<msgs_package::srv::ToWalkingPatternGenerator::Request> request,
+        std::shared_ptr<msgs_package::srv::ToWalkingPatternGenerator::Response> response
+      );
 
-      void callback_res(const rclcpp::Client<msgs_package::srv::ToKinematicsMessage>::SharedFuture future);
+      // void step_WPG_pub(void);
+
+      // void callback_res(const rclcpp::Client<msgs_package::srv::ToKinematicsMessage>::SharedFuture future);
 
 // DEBUG===/*
       void DEBUG_ParameterSetting(void);
--- walking_pattern_generator/src/WalkingPatternGenerator.cpp
+++ walking_pattern_generator/src/WalkingPatternGenerator.cpp
@@ -1,14 +1,17 @@
 #include "rclcpp/rclcpp.hpp"
-#include "rclcpp/qos.hpp"
+// #include "rclcpp/qos.hpp"
 #include <rmw/qos_profiles.h>
 #include "walking_pattern_generator/WalkingPatternGenerator.hpp"
-#include "msgs_package/msg/to_walking_stabilization_controller_message.hpp"
-#include "msgs_package/srv/to_kinematics_message.hpp"
+// #include "msgs_package/msg/to_walking_stabilization_controller_message.hpp"
+// #include "msgs_package/srv/to_kinematics_message.hpp"
+#include "msgs_package/srv/to_walking_pattern_generator.hpp"
+#include "kinematics/IK.hpp"
 
-#include <chrono>
+#include "Eigen/Dense"
+// #include <chrono>
 
 using namespace std::chrono_literals;  // 周期の単位を書けるようにする（ex. 100ms）
-using namespace std::placeholders;  // bind()の第３引数etcを簡単にする（ex. _1）
+// using namespace std::placeholders;  // bind()の第３引数etcを簡単にする（ex. _1）
 
 namespace walking_pattern_generator
 {
@@ -51,117 +54,124 @@ namespace walking_pattern_generator
     walking_pattern_jointVel_L_[2] = {1, 1, 0.5, 1, 0.5, 1};  // [rad/s]
     walking_pattern_jointVel_L_[3] = {1, 1, 0.5, 1, 0.5, 1};
 
-    loop_number_ = walking_pattern_P_R_.max_size();  // 要素の最大数を返す
+    // loop_number_ = walking_pattern_P_R_.max_size();  // 要素の最大数を返す
     // while(rclcpp::ok()){
     // std::cout << loop_number_ << std::endl;
     // }
   }
 
-  void WalkingPatternGenerator::callback_res(
-    const rclcpp::Client<msgs_package::srv::ToKinematicsMessage>::SharedFuture future
-  ) {
-    // DEBUG=====/*
-    // RCLCPP_INFO(this->get_logger(), "RESPONSE: ");
-    // std::cout << "__P_result_R: ";
-    // std::copy(std::begin(future.get()->p_result_r), 
-    //           std::end(future.get()->p_result_r), 
-    //           std::ostream_iterator<double>(std::cout, " "));
-    // std::cout << "\n" << "__P_result_L: ";
-    // std::copy(std::begin(future.get()->p_result_l), 
-    //           std::end(future.get()->p_result_l), 
-    //           std::ostream_iterator<double>(std::cout, " "));
-    // std::cout << "\n" << "__Q_result_R: ";
-    // std::copy(std::begin(future.get()->q_result_r), 
-    //           std::end(future.get()->q_result_r), 
-    //           std::ostream_iterator<double>(std::cout, " "));
-    // std::cout << "\n" << "__Q_result_L: ";
-    // std::copy(std::begin(future.get()->q_result_l), 
-    //           std::end(future.get()->q_result_l), 
-    //           std::ostream_iterator<double>(std::cout, " "));
-    // std::cout << "\n" << std::endl;
-    // DEBUG=====*/
+  // void WalkingPatternGenerator::callback_res(
+  //   const rclcpp::Client<msgs_package::srv::ToKinematicsMessage>::SharedFuture future
+  // ) {
+  //   // DEBUG=====/*
+  //   // RCLCPP_INFO(this->get_logger(), "RESPONSE: ");
+  //   // std::cout << "__P_result_R: ";
+  //   // std::copy(std::begin(future.get()->p_result_r), 
+  //   //           std::end(future.get()->p_result_r), 
+  //   //           std::ostream_iterator<double>(std::cout, " "));
+  //   // std::cout << "\n" << "__P_result_L: ";
+  //   // std::copy(std::begin(future.get()->p_result_l), 
+  //   //           std::end(future.get()->p_result_l), 
+  //   //           std::ostream_iterator<double>(std::cout, " "));
+  //   // std::cout << "\n" << "__Q_result_R: ";
+  //   // std::copy(std::begin(future.get()->q_result_r), 
+  //   //           std::end(future.get()->q_result_r), 
+  //   //           std::ostream_iterator<double>(std::cout, " "));
+  //   // std::cout << "\n" << "__Q_result_L: ";
+  //   // std::copy(std::begin(future.get()->q_result_l), 
+  //   //           std::end(future.get()->q_result_l), 
+  //   //           std::ostream_iterator<double>(std::cout, " "));
+  //   // std::cout << "\n" << std::endl;
+  //   // DEBUG=====*/
 
-    // resultをメンバ変数に記録。FK,IKそれぞれが求めない値（IK->p, FK->q）は、requestで与えた値と同値を返す。
-    p_target_r_ = future.get()->p_result_r;
-    p_target_l_ = future.get()->p_result_l;
-    q_target_r_ = future.get()->q_result_r;
-    q_target_l_ = future.get()->q_result_l;
+  //   // resultをメンバ変数に記録。FK,IKそれぞれが求めない値（IK->p, FK->q）は、requestで与えた値と同値を返す。
+  //   p_target_r_ = future.get()->p_result_r;
+  //   p_target_l_ = future.get()->p_result_l;
+  //   q_target_r_ = future.get()->q_result_r;
+  //   q_target_l_ = future.get()->q_result_l;
     
-    step_counter_++;
-    publish_ok_check_ = true;
+  //   step_counter_++;
+  //   publish_ok_check_ = true;
 
-    // auto time2 = rclcpp::Clock{}.now().seconds();
-    // if(hoge > 16){
-    //   auto time_dev = time2 - time;
-    //   if(time_max < time_dev){time_max = time_dev;}
-    //   if(time_min > time_dev){time_min = time_dev;}
-    //   std::cout << "[WalkingStabilizationController]: " << time_dev << "    max: " << time_max <<  "    min: " << time_min << std::endl;
-    // }
-    // hoge++;
-    // time = time2;
-  }
+  //   // auto time2 = rclcpp::Clock{}.now().seconds();
+  //   // if(hoge > 16){
+  //   //   auto time_dev = time2 - time;
+  //   //   if(time_max < time_dev){time_max = time_dev;}
+  //   //   if(time_min > time_dev){time_min = time_dev;}
+  //   //   std::cout << "[WalkingStabilizationController]: " << time_dev << "    max: " << time_max <<  "    min: " << time_min << std::endl;
+  //   // }
+  //   // hoge++;
+  //   // time = time2;
+  // }
 
-  void WalkingPatternGenerator::step_WPG_pub() {
+  // void WalkingPatternGenerator::step_WPG_pub() {
 
-    // RCLCPP_INFO(this->get_logger(), "step...");
+  //   // RCLCPP_INFO(this->get_logger(), "step...");
 
-    auto toKine_FK_req = std::make_shared<msgs_package::srv::ToKinematicsMessage::Request>();
-    auto toKine_IK_req = std::make_shared<msgs_package::srv::ToKinematicsMessage::Request>();
+  //   auto toKine_FK_req = std::make_shared<msgs_package::srv::ToKinematicsMessage::Request>();
+  //   auto toKine_IK_req = std::make_shared<msgs_package::srv::ToKinematicsMessage::Request>();
 
-    // DEBUG=====
-    toKine_IK_req->r_target_r = {1, 0, 0,
-                                0, 1, 0,
-                                0, 0, 1};
-    toKine_IK_req->p_target_r = walking_pattern_P_R_[step_counter_%loop_number_];  // [m]
-    toKine_IK_req->r_target_l = {1, 0, 0,
-                                0, 1, 0,
-                                0, 0, 1};
-    toKine_IK_req->p_target_l = walking_pattern_P_L_[step_counter_%loop_number_];  // [m]    
-    // DEBUG=====
+  //   // DEBUG=====
+  //   toKine_IK_req->r_target_r = {1, 0, 0,
+  //                               0, 1, 0,
+  //                               0, 0, 1};
+  //   toKine_IK_req->p_target_r = walking_pattern_P_R_[step_counter_%loop_number_];  // [m]
+  //   toKine_IK_req->r_target_l = {1, 0, 0,
+  //                               0, 1, 0,
+  //                               0, 0, 1};
+  //   toKine_IK_req->p_target_l = walking_pattern_P_L_[step_counter_%loop_number_];  // [m]    
+  //   // DEBUG=====
 
-    // FK ERROR_Handling
-    for(int i = 0; i < (int)toKine_FK_req->p_target_r.size(); i++) {
-      if(
-        (std::abs(toKine_FK_req->q_target_r[i]) > 3.14) or
-        (std::abs(toKine_FK_req->q_target_l[i]) > 3.14)
-      ) { 
-        RCLCPP_ERROR(this->get_logger(), "FK_Request: Q_Target: Invalid Value!!");
-        return;
-      }
-    }
-    // IK ERROR_Handling
-    if(
-      (std::abs(toKine_IK_req->p_target_r[2]) > 0.3082)  // コレだと不完全。absがある意味がない。他方向のERROR処理も随時追加
-    ) {
-      RCLCPP_ERROR(this->get_logger(), "IK_Request: P_target: Invalid Value!!");
-    }
+  //   // FK ERROR_Handling
+  //   for(int i = 0; i < (int)toKine_FK_req->p_target_r.size(); i++) {
+  //     if(
+  //       (std::abs(toKine_FK_req->q_target_r[i]) > 3.14) or
+  //       (std::abs(toKine_FK_req->q_target_l[i]) > 3.14)
+  //     ) { 
+  //       RCLCPP_ERROR(this->get_logger(), "FK_Request: Q_Target: Invalid Value!!");
+  //       return;
+  //     }
+  //   }
+  //   // IK ERROR_Handling
+  //   if(
+  //     (std::abs(toKine_IK_req->p_target_r[2]) > 0.3082)  // コレだと不完全。absがある意味がない。他方向のERROR処理も随時追加
+  //   ) {
+  //     RCLCPP_ERROR(this->get_logger(), "IK_Request: P_target: Invalid Value!!");
+  //   }
 
-    // 非同期の待ち状態。待ちつつも、以降のプログラムを実行。このまま（2023/4/1/17:06）だと、responseを受ける前にpublishしてしまう。= resを受け取るより先に以降のプログラムが実行済みになってしまう。ここは、responseを待つ、同期処理にすべき、なのだが、spin_until_future_complete()の引数でthis->...の箇所で、std::runtime_errorを吐いてくる。無理。responseのほうがpublishよりも、約2.4[ms]遅い。  => 強引だが、対策済み
+  //   // 非同期の待ち状態。待ちつつも、以降のプログラムを実行。このまま（2023/4/1/17:06）だと、responseを受ける前にpublishしてしまう。= resを受け取るより先に以降のプログラムが実行済みになってしまう。ここは、responseを待つ、同期処理にすべき、なのだが、spin_until_future_complete()の引数でthis->...の箇所で、std::runtime_errorを吐いてくる。無理。responseのほうがpublishよりも、約2.4[ms]遅い。  => 強引だが、対策済み
 
-    // auto toKine_FK_res = toKine_FK_clnt_->async_send_request(
-    //   toKine_FK_req, 
-    //   std::bind(&WalkingPatternGenerator::callback_res, this, _1)
-    // );
-    // RCLCPP_INFO(this->get_logger(), "Request to kinematics...");
-    auto toKine_IK_res = toKine_IK_clnt_->async_send_request(
-      toKine_IK_req, 
-      std::bind(&WalkingPatternGenerator::callback_res, this, _1)
-    );
+  //   // auto toKine_FK_res = toKine_FK_clnt_->async_send_request(
+  //   //   toKine_FK_req, 
+  //   //   std::bind(&WalkingPatternGenerator::callback_res, this, _1)
+  //   // );
+  //   // RCLCPP_INFO(this->get_logger(), "Request to kinematics...");
+  //   auto toKine_IK_res = toKine_IK_clnt_->async_send_request(
+  //     toKine_IK_req, 
+  //     std::bind(&WalkingPatternGenerator::callback_res, this, _1)
+  //   );
 
-    auto pub_msg = std::make_shared<msgs_package::msg::ToWalkingStabilizationControllerMessage>();
+  //   auto pub_msg = std::make_shared<msgs_package::msg::ToWalkingStabilizationControllerMessage>();
 
-    // set pub_msg
-    pub_msg->p_target_r = p_target_r_;
-    pub_msg->p_target_l = p_target_l_;
-    pub_msg->q_target_r = q_target_r_;
-    pub_msg->q_target_l = q_target_l_;
-    pub_msg->dq_target_r = walking_pattern_jointVel_R_[(step_counter_-1)%loop_number_];
-    pub_msg->dq_target_l = walking_pattern_jointVel_L_[(step_counter_-1)%loop_number_];
+  //   // set pub_msg
+  //   pub_msg->p_target_r = p_target_r_;
+  //   pub_msg->p_target_l = p_target_l_;
+  //   pub_msg->q_target_r = q_target_r_;
+  //   pub_msg->q_target_l = q_target_l_;
+  //   pub_msg->dq_target_r = walking_pattern_jointVel_R_[(step_counter_-1)%loop_number_];
+  //   pub_msg->dq_target_l = walking_pattern_jointVel_L_[(step_counter_-1)%loop_number_];
 
-    if(publish_ok_check_ == true) {
-      toWSC_pub_->publish(*pub_msg);
-      // RCLCPP_INFO(this->get_logger(), "Published...");
-    }
+  //   if(publish_ok_check_ == true) {
+  //     toWSC_pub_->publish(*pub_msg);
+  //     // RCLCPP_INFO(this->get_logger(), "Published...");
+  //   }
+  // }
+
+  void WalkingPatternGenerator::WPG_Server(
+    const std::shared_ptr<msgs_package::srv::ToWalkingPatternGenerator::Request> resuest,
+    std::shared_ptr<msgs_package::srv::ToWalkingPatternGenerator::Response> response
+  ) {
+
   }
 
   WalkingPatternGenerator::WalkingPatternGenerator(
@@ -170,46 +180,46 @@ namespace walking_pattern_generator
 
     // RCLCPP_INFO(this->get_logger(), "Start up WalkingPatternGenerator. Hello WalkingPatternGenerator!!");
 
-    toKine_FK_clnt_ = this->create_client<msgs_package::srv::ToKinematicsMessage>(
-      "FK", 
-      custom_qos_profile
-    );
-    toKine_IK_clnt_ = this->create_client<msgs_package::srv::ToKinematicsMessage>(
-      "IK",
-      custom_qos_profile
-    );
+    // toKine_FK_clnt_ = this->create_client<msgs_package::srv::ToKinematicsMessage>(
+    //   "FK", 
+    //   custom_qos_profile
+    // );
+    // toKine_IK_clnt_ = this->create_client<msgs_package::srv::ToKinematicsMessage>(
+    //   "IK",
+    //   custom_qos_profile
+    // );
 
-    toWSC_pub_ = this->create_publisher<msgs_package::msg::ToWalkingStabilizationControllerMessage>(
-      "WalkingPattern",
-      rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(custom_qos_profile))
-    );
+    // toWSC_pub_ = this->create_publisher<msgs_package::msg::ToWalkingStabilizationControllerMessage>(
+    //   "WalkingPattern",
+    //   rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(custom_qos_profile))
+    // );
     
-    while(!toKine_FK_clnt_->wait_for_service(1s)) {
-      if(!rclcpp::ok()) {
-        RCLCPP_ERROR(this->get_logger(), "ERROR!!: FK service is dead.");
-        return;
-      }
-      // RCLCPP_INFO(this->get_logger(), "Waiting for FK service...");
-    }
-    while(!toKine_IK_clnt_->wait_for_service(1s)) {
-      if(!rclcpp::ok()) {
-        RCLCPP_ERROR(this->get_logger(), "ERROR!!: IK service is dead.");
-        return;
-      }
-      // RCLCPP_INFO(this->get_logger(), "Waiting for IK service...");
-    }
+    // while(!toKine_FK_clnt_->wait_for_service(1s)) {
+    //   if(!rclcpp::ok()) {
+    //     RCLCPP_ERROR(this->get_logger(), "ERROR!!: FK service is dead.");
+    //     return;
+    //   }
+    //   // RCLCPP_INFO(this->get_logger(), "Waiting for FK service...");
+    // }
+    // while(!toKine_IK_clnt_->wait_for_service(1s)) {
+    //   if(!rclcpp::ok()) {
+    //     RCLCPP_ERROR(this->get_logger(), "ERROR!!: IK service is dead.");
+    //     return;
+    //   }
+    //   // RCLCPP_INFO(this->get_logger(), "Waiting for IK service...");
+    // }
 
     // set inital counter value. set walking_pattern.
-    publish_ok_check_ = false;
-    step_counter_ = 0;
+    // publish_ok_check_ = false;
+    // step_counter_ = 0;
 
     // DEBUG: parameter setting
     WalkingPatternGenerator::DEBUG_ParameterSetting();
     
     // Timer処理。指定の周期で指定の関数を実行
-    step_pub_ = this->create_wall_timer(
-      600ms,
-      std::bind(&WalkingPatternGenerator::step_WPG_pub, this)
-    );
+    // step_pub_ = this->create_wall_timer(
+    //   600ms,
+    //   std::bind(&WalkingPatternGenerator::step_WPG_pub, this)
+    // );
   }
 }
