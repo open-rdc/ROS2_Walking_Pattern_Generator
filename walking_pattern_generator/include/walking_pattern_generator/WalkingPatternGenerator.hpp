060a4e1effd1c16eb500b6a81eb48533e01e8451
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2023-04-30 16:36:55 +0900

    Update WPG (Delete unwanted code)

--- walking_pattern_generator/include/walking_pattern_generator/WalkingPatternGenerator.hpp
+++ walking_pattern_generator/include/walking_pattern_generator/WalkingPatternGenerator.hpp
@@ -1,6 +1,4 @@
 #include "rclcpp/rclcpp.hpp"
-// #include "msgs_package/msg/to_walking_stabilization_controller_message.hpp"
-// #include "msgs_package/srv/to_kinematics_message.hpp"
 #include "msgs_package/srv/to_walking_pattern_generator.hpp"
 #include "kinematics/IK.hpp"
 
@@ -13,29 +11,17 @@ namespace walking_pattern_generator
       WalkingPatternGenerator(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
 
     private:
-      // rclcpp::Publisher<msgs_package::msg::ToWalkingStabilizationControllerMessage>::SharedPtr toWSC_pub_;
-      // rclcpp::Client<msgs_package::srv::ToKinematicsMessage>::SharedPtr toKine_FK_clnt_;
-      // rclcpp::Client<msgs_package::srv::ToKinematicsMessage>::SharedPtr toKine_IK_clnt_;
-
       rclcpp::Service<msgs_package::srv::ToWalkingPatternGenerator>::SharedPtr WPG_srv_;
 
       kinematics::IK IK_;
 
-      // get FK, IK result. set publish data.
       std::array<double, 3> p_target_r_;
       std::array<double, 3> p_target_l_;
       std::array<double, 6> q_target_r_;
       std::array<double, 6> q_target_l_;
 
-      // timer
-      // rclcpp::TimerBase::SharedPtr step_pub_;
-
-      // PARAMETER
-      // bool publish_ok_check_;
-      // int step_counter_;
-      // int loop_number_;
       int step_count_;
-      // 逆運動学からJointAngleを導出する
+
       std::array<std::array<double, 3>, 4> walking_pattern_P_R_;
       std::array<std::array<double, 3>, 4> walking_pattern_P_L_;
       std::array<std::array<double, 6>, 4> walking_pattern_jointVel_R_;
@@ -45,10 +31,6 @@ namespace walking_pattern_generator
         const std::shared_ptr<msgs_package::srv::ToWalkingPatternGenerator::Request> request,
         std::shared_ptr<msgs_package::srv::ToWalkingPatternGenerator::Response> response
       );
-
-      // void step_WPG_pub(void);
-
-      // void callback_res(const rclcpp::Client<msgs_package::srv::ToKinematicsMessage>::SharedFuture future);
 
 // DEBUG===/*
       void DEBUG_ParameterSetting(void);
