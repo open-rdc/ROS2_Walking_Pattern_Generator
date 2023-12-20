65fa2ff6cceeb20ee7deccd8230d2ca163c95ced
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2023-12-20 21:26:00 +0900

    bringupにrecorder_launchをinclude.

--- robot_bringup/launch/robot_bringup_launch.py
+++ robot_bringup/launch/robot_bringup_launch.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 
 import launch
 from launch_ros.actions import Node
@@ -6,40 +7,50 @@ from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
+  launch_description = launch.LaunchDescription()
+
+  mode_switch_yaml_path = os.path.join(get_package_share_directory("robot_bringup"), "config", "param_mode_switch.yaml")
+  with open(mode_switch_yaml_path, "r") as f:
+    mode_switch_yaml = yaml.safe_load(f)["/**"]["ros__parameters"]["mode_switch"]
 
   # load simulation launch file
-  sim_launch = launch.actions.IncludeLaunchDescription(
-    PythonLaunchDescriptionSource([
-      os.path.join(get_package_share_directory("webots_robot_handler"), "launch"), 
-      "/start_launch.py"
-    ])
-  )
+  if mode_switch_yaml["using_simulator"] == True:
+    sim_launch = launch.actions.IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([
+        os.path.join(get_package_share_directory("webots_robot_handler"), "launch"), 
+        "/start_launch.py"
+      ])
+    )
+    launch_description.add_action(sim_launch)
 
-  # Load visualizer launch file
-  visual_launch = launch.actions.IncludeLaunchDescription(
-    PythonLaunchDescriptionSource([
-      os.path.join(get_package_share_directory("robot_visualizer"), "launch"), 
-      "/robot_visualizer.launch.py"
-    ])
-  )
+  # Load visualizer launch file & recorder launch file
+  if mode_switch_yaml["debug_mode"] == True:
+    visual_launch = launch.actions.IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([
+        os.path.join(get_package_share_directory("robot_visualizer"), "launch"), 
+        "/robot_visualizer.launch.py"
+      ])
+    )
+    #launch_description.add_action(visual_launch)
+    record_launch = launch.actions.IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([
+        os.path.join(get_package_share_directory("robot_recorder"), "launch"),
+        "/robot_recorder.launch.py"
+      ])
+    )
+    launch_description.add_action(record_launch)
 
   # Logger launch fileはRMの起動をしているlaunchで読み込み、起動をするべき。動いていないときにどう記録しろと。
 
   # load robot_manager node
   # TODO: RMの起動など、制御プログラムは他launchファイルに記述して、制御実行時にそのlaunchファイルを起動する手順を取るべき。
     # そうしないと、rosbagを使って再現するときに干渉しちゃうし、RMの起動を阻止しないと行けない。
-  robot_manager = Node(
-    package = "robot_manager",
-    executable = "robot_manager",
-    output = "screen"
-  )
+  # robot_manager = Node(
+  #   package = "robot_manager",
+  #   executable = "robot_manager",
+  #   output = "screen"
+  # )
+  # launch_description.add_action(robot_manager)
 
   # Execution
-  return launch.LaunchDescription([
-    # another launch file
-    sim_launch,
-    visual_launch,
-
-    # node
-    robot_manager
-  ])
+  return launch_description
---
+++ robot_recorder/launch/robot_recorder.launch.py
@@ -0,0 +1,85 @@
+# Logger launch file
+
+# TODO: rosbag2の仕様確認と、実行
+  # 開発ソフトウェアに関わるTopicをrecord。他はいらん。recordするTopic名をconfig fileから読み取るか？User側で増やしたくなるかもしれんし、Configいじれば良いんだな！という思考の流れを持つだろうから。
+
+import os 
+import datetime
+import yaml
+
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+  launch_description = LaunchDescription()
+
+  debug_mode_yaml_path = os.path.join(get_package_share_directory("robot_bringup"), "config", "param_debug_mode.yaml")
+  with open(debug_mode_yaml_path, "r") as f:
+    debug_mode_yaml = yaml.safe_load(f)["/**"]["ros__parameters"]["setting_debug_mode"]
+
+  launch_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+  #record_dir_path = "./src/ROS2_Walking_Pattern_Generator/Record/" + launch_datetime
+  record_dir_path = "./src/Records/" + launch_datetime  # DEBUG
+
+  try:
+    os.mkdir(record_dir_path)
+  except:
+    None
+
+  record_dir_path = record_dir_path + "/"
+
+  recorder_params = {
+      "record_dir_path": record_dir_path, 
+      "launch_datetime": launch_datetime
+  }
+
+  if debug_mode_yaml["using_feedback_recorder"] == True:
+    feedback_recorder_node = Node(
+      package = "robot_recorder",
+      executable = "feedback_recorder",
+      parameters = [recorder_params],
+    )
+    launch_description.add_action(feedback_recorder_node)
+
+  if debug_mode_yaml["using_feedback_recorder"] == True:
+    footStep_recorder_node = Node(
+      package = "robot_recorder",
+      executable = "footStep_recorder",
+      parameters = [recorder_params],
+    )
+    launch_description.add_action(footStep_recorder_node)
+
+  if debug_mode_yaml["using_walkingPattern_recorder"] == True:
+    walkingPattern_recorder_node = Node(
+      package = "robot_recorder",
+      executable = "walkingPattern_recorder",
+      parameters = [recorder_params],
+    )
+    launch_description.add_action(walkingPattern_recorder_node)
+
+  if debug_mode_yaml["using_walkingStabilization_recorder"] == True:
+    walkingStabilization_recorder_node = Node(
+      package = "robot_recorder",
+      executable = "walkingStabilization_recorder",
+      parameters = [recorder_params],
+    )
+    launch_description.add_action(walkingStabilization_recorder_node)
+
+  if debug_mode_yaml["using_jointState_recorder"] == True:
+    jointState_recorder_node = Node(
+      package = "robot_recorder",
+      executable = "jointState_recorder",
+      parameters = [recorder_params],
+    )
+    launch_description.add_action(jointState_recorder_node)
+
+  if debug_mode_yaml["using_rosbag2"] == True:
+    None
+
+
+  return launch_description
