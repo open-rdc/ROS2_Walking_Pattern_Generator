14c79485416f66eaca8301f0caf6b1b63eaa3346
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2023-12-24 17:20:25 +0900

    Add param FootStepPlanner

--- foot_step_planner/include/foot_step_planner/FootStepPlanner.hpp
+++ foot_step_planner/include/foot_step_planner/FootStepPlanner.hpp
@@ -6,11 +6,16 @@ namespace foot_step_planner
   class Default_FootStepPlanner : public control_plugin_base::FootStepPlanner
   {
     public:
-      Default_FootStepPlanner(){}
+      Default_FootStepPlanner();
       ~Default_FootStepPlanner(){}
       
       std::unique_ptr<control_plugin_base::FootStep> foot_step_planner(void) override;
 
     private:
+      rclcpp::Node::SharedPtr node_ptr_;
+      std::shared_ptr<rclcpp::SyncParametersClient> client_param_;
+
+      double WALKING_CYCLE_ = 0;
+      double WAIST_HEIGHT_ = 0;
   };
 }
--- foot_step_planner/src/FootStepPlanner.cpp
+++ foot_step_planner/src/FootStepPlanner.cpp
@@ -5,7 +5,7 @@ namespace foot_step_planner
   std::unique_ptr<control_plugin_base::FootStep> Default_FootStepPlanner::foot_step_planner(void) {
     auto foot_step_ptr = std::make_unique<control_plugin_base::FootStep>();
     
-    foot_step_ptr->walking_step_time = 0.8;  // 歩行周期[s]
+    foot_step_ptr->walking_step_time = WALKING_CYCLE_;  // 歩行周期[s]
     foot_step_ptr->foot_pos = {  // 着地位置{x, y}[m]
       {0.0, 0.037},
       {0.0, 0.074},
@@ -16,11 +16,20 @@ namespace foot_step_planner
       {0.12, 0.037},
       {0.12, 0.037}
     };
-    foot_step_ptr->waist_height = 171.856 / 1000;  // 腰高さ
+    foot_step_ptr->waist_height = WAIST_HEIGHT_;
+    //foot_step_ptr->waist_height = 171.856 / 1000;  // 腰高さ
 
     // std::cout << "Here is default foot_step_controller plugin." << std::endl;
 
     return foot_step_ptr;
+  }
+
+  Default_FootStepPlanner::Default_FootStepPlanner() {
+    node_ptr_ = rclcpp::Node::make_shared("FootStepPlanner");
+    client_param_ = std::make_shared<rclcpp::SyncParametersClient>(node_ptr_, "RobotParameterServer");
+
+    WALKING_CYCLE_ = client_param_->get_parameter<double>("control_times.walking_cycle");
+    WAIST_HEIGHT_ = client_param_->get_parameter<double>("control_constant.waist_pos_z");
   }
 }
 
